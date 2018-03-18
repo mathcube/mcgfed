3dab13428894096bd6e5af6639e75f525a32d6be
mathcube <cordiusmacrobius@gmail.com>
2018-03-18 08:25:03 -0500

    fix defines file

--- common/defines/sc_defines.lua
+++
@@ -1,4 +0,0 @@
--- Note: for modding, avoid overwriting this file - instead overwrite individual
--- keys from another lua file using the format NDefines.NCamera.FOV = 50
-
-NDefines.NGameplay.FEDERATION_USES_LEADER_ROTATION = 0
